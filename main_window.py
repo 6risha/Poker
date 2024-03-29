e3a7c5ce5892ce9cd54eee7b3812c7720bf1d425
Comas Gaston <gcomas@pc107-042-24.insa-lyon.fr>
2024-03-29 17:42:10 +0100

    Second try

--- main_window.py
+++ main_window.py
@@ -5,6 +5,9 @@ class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Poker")
+        
+        self.label = tk.Label(self, text='Bonjour !')
+        self.label.pack()
 
 if __name__ == "__main__":
     app = MainWindow()

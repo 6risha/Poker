d646e73a72fa5f0aaaeb6c3c3c5839002fad5f77
Grisha <6risha@Grishas-MacBook-Pro.local>
2024-05-20 14:15:05 +0200

    community cards

--- bot/cfr-learn/train-HUNL.py
+++ bot/cfr-learn/train-HUNL.py
@@ -1,31 +1,26 @@
-# The training is done for the preflop stage only
-# The training is done for the bot being small blind
-
+import numpy as np
+import itertools
+import joblib
+from tqdm import tqdm
 from cards import *
 
-import numpy as np
-from random import shuffle
-from tqdm import tqdm
-import joblib
-
 # Poker definitions
-# The bot is trained as if its opponent is user
 game = Game()
 global player
+global opponent
 players = [game.bot, game.user]
 
 FOLD = ('fold', 0, 'p')
 CHECK = ('check', 0, 'p')
-CALL = ('call', 0, 'c')  # 0 is changed in the game to the right bet
-RAISE_1BB = ('raise', max(game.bot.bet, game.user.bet) + game.big_blind, 'b')
-RAISE_2BB = ('raise', max(game.bot.bet, game.user.bet) + game.big_blind * 2, 'b')
-RAISE_4BB = ('raise', max(game.bot.bet, game.user.bet) + game.big_blind * 4, 'b')
+CALL = ('call', abs(game.bot.bet - game.user.bet), 'c')
+RAISE_1BB = ('raise', abs(game.bot.bet - game.user.bet) + game.big_blind, 'b')
+RAISE_2BB = ('raise', abs(game.bot.bet - game.user.bet) + game.big_blind * 2, 'b')
+RAISE_4BB = ('raise', abs(game.bot.bet - game.user.bet) + game.big_blind * 4, 'b')
 ALL_IN = ('raise', game.user.chips if player == game.user else game.bot.chips, 'b')
 
 ACTIONS = [FOLD, CHECK, CALL, RAISE_1BB, RAISE_2BB, RAISE_4BB, ALL_IN]
 NUM_ACTIONS = 7
 
-# Initialize TreeMap
 TreeMap = {}
 
 
@@ -47,7 +42,7 @@ class Node:
         pass
 
 
-def cfr(cards, history, p0, p1):
+def cfr(community_cards, hole_cards, history, p0, p1):
     # TODO
     player = players[len(history) % 2]
     opponent = players[1 - player]
@@ -66,6 +61,8 @@ def cfr(cards, history, p0, p1):
     elif len(history) == 1:
         if history[0] == 'p':  # fold
             pass
+
+    player_hand = []
 
     infoSet = ''.join(str(card) for card in player.hole_cards + history)
     if infoSet not in TreeMap:
@@ -90,8 +87,7 @@ def cfr(cards, history, p0, p1):
             pass
 
 
-# Train the model
-def train(iterations):
+def train_preflop(iterations):
     util = 0
     for i in tqdm(range(iterations), desc="Training Loop"):
         cards = random.sample(game.deck, 4)
@@ -101,10 +97,28 @@ def train(iterations):
     print("Training complete.")
 
 
+def train(iterations, num_cards=3):
+    utils = []
+    community_combinations = list(itertools.combinations(game.deck, num_cards))
+    for community_cards in tqdm(community_combinations, desc="Community Cards Combinations"):
+        util = 0
+        selected_set = set(community_cards)
+        remaining_cards = [card for card in game.deck if card not in selected_set]
+        for _ in tqdm(range(iterations), desc="Training Loop", leave=False):
+            hole_cards = random.sample(remaining_cards, 4)
+            util += cfr(community_cards, hole_cards, '', 1, 1)
+        utils.append(util)
+    avg_util = np.mean(utils)
+    print(f"Average Utility: {avg_util}")
+
+
 if __name__ == "__main__":
     train_from_scratch = False
     if train_from_scratch:
-        train(1_000_000)
+        train_preflop(1_000_000)
+        train(1200, 3)
+        train(800, 4)
+        train(400, 5)
         joblib.dump(TreeMap, "HUNL-TreeMap.joblib")
     else:
         TreeMap = joblib.load("HUNL-TreeMap.joblib")
